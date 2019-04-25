e4bac4ecbe62715f1aa3ebc5203275b20d82cfba
Kevin Kamanda <kevkamanda@gmail.com>
2019-04-25 10:38:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,16 @@ class Dog
     @name
   end
   
+  def breeds
+    @breeds
+  end
+  
   def name=(new_name)
     @name = new_name
   end
   
-  def breeds
-  
+  def breeds=(new_breeds)
+    @breeds = new_breeds  
   end
   
 end
